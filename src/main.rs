f4ef31f210c7fba42ce81bdb09709e0b1e5813ad
Alexander Ermakov <ermakovav@altgtu.ru>
2026-06-11 10:24:23 +0700

    новые правила

--- src/main.rs
+++ src/main.rs
@@ -41,94 +41,98 @@ fn main() -> Result<ExitCode, Box<dyn Error>> {
 
         // C#
         // TODO  set { weightKg = value >= 0 ? value : 0; }
-        if let Some(idx) = line.trim().find("//")
-            && idx > 0
-        {
-            warn!("{}: {line}", line_num + 1);
-        }
-        if line.contains("=>") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("??=") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("this(") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("===") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("---") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("tring('='") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("tring('-'") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        // Console.WriteLine($"Vector({X:F1}, {Y:F1}) | Length = {Length():F3}");
-        if line.contains(":F3}") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("Console.WriteLine(\"[") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("\\\"") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains(".PadRight(") {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
-        if line.contains("1. ")
+        let is_detect = line
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(4)
+            .any(|window| window.iter().all(|&c| c.is_uppercase()))
+            || line.contains("=>")
+            || line.contains("??=")
+            || line.contains("this(")
+            || line.contains("===")
+            || line.contains("---")
+            || line.contains("tring('='")
+            || line.contains("tring('-'")
+            || line.contains("Console.WriteLine(\"[")
+            || line.contains("\\\"")
+            || line.contains(":F1}")
+            || line.contains(":F2}")
+            || line.contains(":F3}")
+            || line.contains(":F4}")
+            || line.contains(":F5}")
+            || line.contains(":F6}")
+            || line.contains("1. ")
             || line.contains("2. ")
             || line.contains("3. ")
             || line.contains("4. ")
             || line.contains("5. ")
             || line.contains("6. ")
+            || line.contains("var")
+            || line.contains("var")
+            || line.contains("select")
+            || line.contains("from")
+            || line.contains("where")
+            || line.contains("orderby")
+            || line.contains("System.Linq")
+            || line.contains(".Select(")
+            || line.contains(".Where(")
+            || line.contains(".OrderBy(")
+            || line.contains(".OrderByDescending(")
+            || line.contains(".ThenBy(")
+            || line.contains(".ThenByDescending(")
+            || line.contains(".Join(")
+            || line.contains(".Aggregate(")
+            || line.contains(".ToLookup(")
+            || line.contains(".GroupJoin(")
+            || line.contains(".Reverse(")
+            || line.contains(".All(")
+            || line.contains(".Any(")
+            || line.contains(".Contains(")
+            || line.contains(".Distinct(")
+            || line.contains(".Except(")
+            || line.contains(".Union(")
+            || line.contains(".Intersect(")
+            || line.contains(".Count(")
+            || line.contains(".Sum(")
+            || line.contains(".Average(")
+            || line.contains(".Min(")
+            || line.contains(".Max(")
+            || line.contains(".Take(")
+            || line.contains(".Skip(")
+            || line.contains(".TakeWhile(")
+            || line.contains(".SkipWhile(")
+            || line.contains(".Concat(")
+            || line.contains(".Zip(")
+            || line.contains(".First(")
+            || line.contains(".FirstOrDefault(")
+            || line.contains(".Single(")
+            || line.contains(".SingleOrDefault(")
+            || line.contains(".ElementAt(")
+            || line.contains(".ElementAtOrDefault(")
+            || line.contains(".Last(")
+            || line.contains(".LastOrDefault(")
+            || line.contains(".PadRight(");
+
+        if is_detect {
+            warn!("{}: {line}", line_num + 1);
+            count_warn += 1;
+        }
+        if let Some(idx) = line.trim().find("//")
+            && idx > 0
         {
             warn!("{}: {line}", line_num + 1);
             count_warn += 1;
         }
 
-        if line
-            .chars()
-            .collect::<Vec<char>>()
-            .windows(4)
-            .any(|window| window.iter().all(|&c| c.is_uppercase()))
-        {
-            warn!("{}: {line}", line_num + 1);
-            count_warn += 1;
-        }
+        let is_detect = line.contains("Console.WriteLine(\"\\n")
+            || line.contains("()}")
+            || line.contains("new(")
+            || line.contains("<summary>");
 
-        if line.contains("Console.WriteLine(\"\\n") {
+        if is_detect {
             info!("{}: {line}", line_num + 1);
             count_info += 1;
         }
-        // {consumer.TotalPayment()}"
-        if line.contains("()}") {
-            info!("{}: {line}", line_num + 1);
-            count_info += 1;
-        }
-        if line.contains("new(") {
-            info!("{}: {line}", line_num + 1);
-            count_info += 1;
-        }
-        // if line.contains("<summary>") {
-        //     info!("{}: {line}", line_num + 1);
-        //     count_info += 1;
-        // }
     }
 
     info!("\nwarnings={count_warn} info={count_info}");
